c62aae66006b6deadfd2accf3808a2080ac81b70
s4rduk4r@gmail.com <you@example.com>
2022-03-04 21:09:59 +0300

    max_rows() with argument list

--- preprocessing/series.py
+++ preprocessing/series.py
@@ -1,17 +1,20 @@
 import pandas as pd
 
-def max_rows(s1 : pd.Series, s2 : pd.Series) -> pd.Series:
+def max_rows(*series : pd.Series) -> pd.Series:
     """Нахождение максимума между элементами рядов pandas.Series
     
     Оба ряда должны иметь одинаковые индексы
 
     Args:
-        s1 (pd.Series): Первый ряд сравнения
-        s2 (pd.Series): Второй ряд сравнения
+        *series (pd.Series): Ряды сравнения
 
     Returns:
         pd.Series: Ряд, содержащий максимум для данной строки
     """
+    d = dict()
+    for s in range(len(series)):
+        d[s] = series[s]
+        
     return pd.DataFrame(
-        data = {"s1" : s1, "s2" : s2}
+        data = d
     ).max(axis = 1)
