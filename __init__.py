7519e3cf9f474a2162b8c7ad364b6017efb3127b
s4rduk4r@gmail.com <you@example.com>
2022-03-13 14:31:13 +0300

    Entry level lib imports to shorten in-code imports

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
 __all__ = ["asserts", "pretty_msg"]
 
 __version__ = "0.0.1"
+
+import masala.asserts
+import masala.pretty_msg
+
+assert_step = masala.asserts.assert_step
+print_error = masala.pretty_msg.print_error
+print_msg = masala.pretty_msg.print_msg
+print_warn = masala.pretty_msg.print_warn
+
+import masala.preprocessing.dataframe
+open_dataset = masala.preprocessing.dataframe.open_dataset
+na_counts_and_props = masala.preprocessing.dataframe.na_counts_and_props
