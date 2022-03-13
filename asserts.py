7996882aef961b2be53f23353a27919d881efe31
s4rduk4r@gmail.com <you@example.com>
2022-03-13 16:20:12 +0300

    Add assert_step2 method to designate reason explicitly

--- asserts.py
+++ asserts.py
@@ -1,14 +1,27 @@
 from masala.pretty_msg import print_error
 
-def assert_step(value, expected = True) -> None:
+def assert_step(expression : bool) -> None:
     """Обвязка try...except конструкции вокруг стандартного assert
 
     Args:
-        value (_type_): проверяемое значение
-        expected (bool, optional): ожидаемое значение. Defaults to True.
+        expression (bool)   :   выражение для assert
+        err_msg (str)       :   сообщение о природе ошибки
     """
     try:
-        assert value == expected
+        assert expression
     except AssertionError as err:
         print_error(err)
+    
+    
+def assert_step2(expression : bool, error_to_print : Exception) -> None:
+    """Обвязка try...except конструкции вокруг стандартного assert
+
+    Args:
+        expression (bool): выражение для assert
+        error_to_print (Exception): сообщение о природе ошибки
+    """
+    try:
+        assert expression
+    except AssertionError:
+        print_error(error_to_print)
     
