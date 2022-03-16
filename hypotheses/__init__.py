e542a4337b73bcba567796eb1dae8f06eb11c541
s4rduk4r@gmail.com <you@example.com>
2022-03-16 09:20:25 +0300

    Revert "Function aliases"
    This reverts commit c7ffbf5e60a5b672033959af4553a5c32d1141bd.

--- hypotheses/__init__.py
+++ hypotheses/__init__.py
@@ -1,7 +1,1 @@
-import masala.hypotheses.categorical
-
 __all__ = ["categorical"]
-
-prop_conf_int = masala.hypotheses.categorical.prop_conf_int
-multiple_comparisons_chi2 = masala.hypotheses.categorical.multiple_comparisons_chi2
-pairwise_fisher = masala.hypotheses.categorical.pairwise_fisher
